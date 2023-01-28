c3ff97a2f36562c8eb29cdf0ebf371be56cb62f4
zerom-H <cupdisin@gmail.com>
2023-01-29 00:06:04 +0900

    add: design-browser-history.py 문제 입력

---
+++ algorithm/leetcode/design-browser-history.py
@@ -0,0 +1,21 @@
+class BrowserHistory(object):
+    def __init__(self, homepage):
+        """
+        :type homepage: str
+        """
+
+    def visit(self, url):
+        """
+        type url: str
+        rtype: None
+        """
+    def back(self, steps):
+        """
+        type steps: int
+        rtype: str
+        """
+    def forward(self, steps):
+        """
+        type steps: int
+        rtype: str
+        """
