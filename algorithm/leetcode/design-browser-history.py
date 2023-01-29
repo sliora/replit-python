61a9dcf7a6f8b4200895294e73547e80a9658a81
zerom-H <cupdisin@gmail.com>
2023-01-30 00:00:20 +0900

    add: forward 구현 및 최종 완료

--- algorithm/leetcode/design-browser-history.py
+++ algorithm/leetcode/design-browser-history.py
@@ -18,18 +18,23 @@ class BrowserHistory(object):
             self.current = self.current.prev
         return self.current.val
     def forward(self, steps):
-        """
-        type steps: int
-        rtype: str
-        """
+        while steps > 0 and self.current.next is not None:
+            steps -= 1
+            self.current = self.current.next
+        return self.current.val
 
 
 history = BrowserHistory("leetcode.com")
 history.visit("google.com")
 history.visit("facebook.com")
 history.visit("youtube.com")
-print(history.head.next.val)
-print(history.current.next)
+history.back(1)
+history.back(1)
+history.forward(1)
+history.visit("linkedin.com")
+history.forward(2)
+history.back(2)
+history.back(7)
+
 print(history.current.val)
-print(history.current.prev.val)
-print(history.back(5))
+
