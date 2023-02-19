571ebb3648883e5bf31a1c03aaaaf88a524dc484
zerom-H <cupdisin@gmail.com>
2023-02-20 00:50:07 +0900

    add: valid-parentheses.py 구현

---
+++ algorithm/leetcode/valid-parentheses.py
@@ -0,0 +1,26 @@
+"""
+Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+An input string is valid if:
+
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
+Every close bracket has a corresponding open bracket of the same type.
+"""
+def isValid(s):
+    stack = []
+    for p in s:
+        if p == "(":
+            stack.append(")")
+        elif p == "{":
+            stack.append("}")
+        elif p == "[":
+            stack.append("]")
+        elif not stack or stack.pop() != p:
+            return False
+    return not stack
+
+
+print(isValid("{(([]))[]}"))
+
+
